2a3d1f912b23e8e07ed5d50ac9a7a4854ba87bae
sundrie <sundrie42@gmail.com>
2017-03-20 11:47:31 +0100

    un peu de css pour mettre en forme la liste

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Champ de recherche</title>
-
+    <link rel="stylesheet" href="static/css/master.css">
     <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
     <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
