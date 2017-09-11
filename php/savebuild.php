24e79e706de32ca55ffd556abe77abe41c82f555
sundrie <sundrie42@gmail.com>
2017-09-11 19:58:33 +0200

    Le fichier .txt se créé bien là ou on veut, le nom du fichier sera a changer plus tard (changer la localisation selon votre config PC)

--- php/savebuild.php
+++ php/savebuild.php
@@ -1,16 +1,20 @@
 <?php
-//if($_POST){
+//********* veut dire à enlever une fois tests finis
+
+//********* if($_POST){
   // Pour les test on masque mais une fois ceux finis ont le réaffichera et supprimera la 'fake' variable
-  // $build = $_POST['build'];
+  //********** $build = $_POST['build'];
   $build = array ('Chrom','Aether','(Tec/2)% de faire 2 attaques consécutives, la première bénéficie de Sol et la seconde de Luna','Rightful King',"Augmente de 10% les chances d'activer les talents",'Aegis','(Tec)% Dégâts reçus par Arcs, Tomes et Dracopierres /2','Resistance+10','Resistance+10','Aggressor','Attaque+10 pendant le tour du joueur');
   // Pour avoir un retour sur notre page (data) à enlever une fois en ligne
   var_dump($build);
   // code w3C à modifié plus tard
-  // $myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
-  // $txt = "John Doe\n";
-  // fwrite($myfile, $txt);
-  // $txt = "Jane Doe\n";
-  // fwrite($myfile, $txt);
-  // fclose($myfile);
-//}
+  //******** La localisation sera a changer (propre à ma config PC) c'est juste pour le confort des tests
+  $myfile = fopen("E://Downloads/test.txt", "w") or die("Unable to open file!");
+  $txt = "John Doe\n";
+  fwrite($myfile, $txt);
+  $txt = "Jane Doe\n";
+  fwrite($myfile, $txt);
+  fclose($myfile);
+
+//******** }
 ?>
