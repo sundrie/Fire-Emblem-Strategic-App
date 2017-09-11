9742b1b6eda1bc831ac18e86490c1f8e23e3d078
sundrie <sundrie42@gmail.com>
2017-09-11 19:47:28 +0200

    On passe au PHP dans un premier temps on a créé notre fake variable  pour les test

--- php/savebuild.php
+++ php/savebuild.php
@@ -1,6 +1,8 @@
 <?php
-if($_POST){
-  $build = $_POST['build'];
+//if($_POST){
+  // Pour les test on masque mais une fois ceux finis ont le réaffichera et supprimera la 'fake' variable
+  // $build = $_POST['build'];
+  $build = array ('Chrom','Aether','(Tec/2)% de faire 2 attaques consécutives, la première bénéficie de Sol et la seconde de Luna','Rightful King',"Augmente de 10% les chances d'activer les talents",'Aegis','(Tec)% Dégâts reçus par Arcs, Tomes et Dracopierres /2','Resistance+10','Resistance+10','Aggressor','Attaque+10 pendant le tour du joueur');
   // Pour avoir un retour sur notre page (data) à enlever une fois en ligne
   var_dump($build);
   // code w3C à modifié plus tard
@@ -10,5 +12,5 @@ if($_POST){
   // $txt = "Jane Doe\n";
   // fwrite($myfile, $txt);
   // fclose($myfile);
-}
+//}
 ?>
