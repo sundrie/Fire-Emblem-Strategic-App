accd6230edaa69b1caae0a4559b77f82d25041b7
unknown <sundrie42@gmail.com>
2018-02-19 17:27:17 +0100

    Ajout du squelette de base à la page pour me contacter (header,body, footer,etc)

--- FireEmblemStrategicApp/pages/contactme.php
+++ FireEmblemStrategicApp/pages/contactme.php
@@ -1,1 +1,30 @@
-<p>Hello World</p>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" href="http://alexandreblin.ovh/FireEmblemStrategicApp/static/css/normalize.css">
+		<link rel="stylesheet" href="http://alexandreblin.ovh/FireEmblemStrategicApp/static/css/master.css">
+		<link href="http://alexandreblin.ovh/FireEmblemStrategicApp/static/css/fontawesome-all.min.css" rel="stylesheet">
+		<title>Fire Emblem Strategic App | Me contacter</title>
+	</head>
+	<body>
+    <header class="headerwebsite headerwebsiteabout">
+      <a href="http://alexandreblin.ovh/index.html"><img src="http://alexandreblin.ovh/FireEmblemStrategicApp/static/img/FireEmblemLogo - Menu.png" alt="Retour Accueil" class="ImgBackHome"></a>
+      <h1>Fire Emblem Strategic App \ Me contacter</h1>
+    </header>
+    <div class="wrapper">
+      
+    </div>
+		<footer>
+      <p>Made with love by Alexandre Blin</p>
+      <p>
+				Retrouvez moi sur :
+        <a href="https://www.linkedin.com/in/alexandre-blin-bb637ab7/"><i class="fab fa-linkedin"></i></a>
+				<a href="https://github.com/sundrie"><i class="fab fa-github-square"></i></a>
+				<a href="https://www.facebook.com/alexandre.blin.18"><i class="fab fa-facebook-square"></i></a>
+      </p>
+			<p><a href="http://alexandreblin.ovh/FireEmblemStrategicApp/pages/contactme.php">Me contacter</a></p>
+    </footer>
+	</body>
+</html>
