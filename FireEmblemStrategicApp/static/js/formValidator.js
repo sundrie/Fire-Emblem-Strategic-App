784ed8cd79a53c4df0e41147c60ba4bfd1e3890b
unknown <sundrie42@gmail.com>
2018-02-23 19:10:46 +0100

    Déplacement de notre bout de code pour la vérif du reCAPTCHA et l'envoi du mail dans le if qui a vérifié l'adresse mail et quelle est correcte

--- FireEmblemStrategicApp/static/js/formValidator.js
+++ FireEmblemStrategicApp/static/js/formValidator.js
@@ -8,30 +8,31 @@ $(function() {
     // Si l'adresse mail est bonne
     if (emailcheck.test($("input[name='email']").val())==true) {
       console.log("adresse mail valide");
+      // Si l'utilisateur n'a pas fait le test on lui affiche un message l'invitant à faire le test
+      // Renvoie '' (rien) si test pas fait ou la clé reCAPTCHA de notre site si test fait
+      if (grecaptcha.getResponse() === '') {
+        $("#message").html("<p><h2>Veuillez valider le reCAPTCHA pour vérifier que vous n'êtes pas un robot s'il vous plaît</h2></p>");
+      } else {
+        $.ajax({
+          url: 'http://alexandreblin.ovh/FireEmblemStrategicApp/pages/contactme.php',
+          method: 'POST',
+          data:{
+            objmsg: $("input[name='objectmsg']").val(),
+            email: $("input[name='email']").val(),
+            textmsg: $("textarea[name='textmsg']").val()
+          },
+          success: function(data){
+            console.log(data);
+            $("#message").html("<p><h2>Votre message a été correctement envoyé</h2></p>");
+          }
+        });
+      }
+
+    });
     } else {
       $("#message").html("<p><h2>Adresse email invalide</h2></p>");
     }
-    // Si l'utilisateur n'a pas fait le test on lui affiche un message l'invitant à faire le test
-    // Renvoie '' (rien) si test pas fait ou la clé reCAPTCHA de notre site si test fait
-    if (grecaptcha.getResponse() === '') {
-      $("#message").html("<p><h2>Veuillez valider le reCAPTCHA pour vérifier que vous n'êtes pas un robot s'il vous plaît</h2></p>");
-    } else {
-      // $.ajax({
-      //   url: 'http://alexandreblin.ovh/FireEmblemStrategicApp/pages/contactme.php',
-      //   method: 'POST',
-      //   data:{
-      //     objmsg: $("input[name='objectmsg']").val(),
-      //     email: $("input[name='email']").val(),
-      //     textmsg: $("textarea[name='textmsg']").val()
-      //   },
-      //   success: function(data){
-      //     console.log(data);
-      //     $("#message").html("<p><h2>Votre message a été correctement envoyé</h2></p>");
-      //   }
-      // });
-    }
 
-  });
 
 
 });
