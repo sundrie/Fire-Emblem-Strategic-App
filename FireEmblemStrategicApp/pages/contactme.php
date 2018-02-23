b72471ab7aab1d12edad36a110093ac274225f32
unknown <sundrie42@gmail.com>
2018-02-23 20:23:07 +0100

    css contactme.php : troisième ligne du formulaire traitée

--- FireEmblemStrategicApp/pages/contactme.php
+++ FireEmblemStrategicApp/pages/contactme.php
@@ -20,7 +20,7 @@
 	      <form action="http://alexandreblin.ovh/FireEmblemStrategicApp/pages/contactme.php" method="post" id="contactForm" class="formulaireContact">
 	        <p class="p-objectmsg">Objet de votre message : <input type="text" name="objectmsg" class="class-objectmsg" required></p>
 					<p class="p-email">Votre adresse mail pour que je vous recontacte si besoin : <input type="email" name="email" class="class-email" required></p>
-	        <p class="">Votre message : <textarea name="textmsg" rows="8" cols="80" required></textarea></p>
+	        <p class="p-textmsg">Votre message : <textarea name="textmsg" class="class-textmsg" rows="8" cols="80" required></textarea></p>
 					<div class="g-recaptcha" data-sitekey="6LeCAkgUAAAAAM1LmVGYFYnL8hkpOjg_fjDBeZs-"></div>
 	        <p><input type="submit" value="Envoyer"></p>
 	      </form>
