9286137d30c91e4d968005ed3d23a8192fc6fd7d
unknown <sundrie42@gmail.com>
2018-03-22 21:49:19 +0100

    Voilà notre fonction est opérationelle on a une variable array qui récupère tous les talents correspondant aux classes que le héros peut avoir

--- FireEmblemStrategicApp/Data-FireEmblemAwakening/Tools/test.js
+++ FireEmblemStrategicApp/Data-FireEmblemAwakening/Tools/test.js
@@ -6,17 +6,21 @@ $(document).ready(function(){
     success: function(data){
       // Variable qui liste toutes les classes
       var allclasses = $(data).children().children();
-      console.log(allclasses)
-      var classes
+      // console.log(allclasses)
+      // On déclare une variable de type Array pour pouvoir utiliser la méthode push()
+      var classes = [];
+      // Pour l'exemple j'utilise les classes de Chrom
+      var HeroClassList = ["Lord","Great Lord","Cavalier","Paladin","Great Knight","Archer","Sniper","Bow Knight","Dread Fighter"];
       // On boucle sur chaque classes
       $(allclasses).each(function(i) {
-        // Si il trouve l'attribut name (correspond dans le fichier xml à <class name="">) alors il le stocke dans la variable
-        if ($(this).attr("name") === "Lord") {
-          // console.log($(this))
-          classes = $(this);
+        // Si il trouve l'attribut name (correspond dans le fichier xml à <class name="">) alors il le stocke dans la variable classes
+        if($.inArray($(this).attr("name"), HeroClassList) !== -1) {
+          console.log($(this))
+          classes.push($(this));
         }
       });
       // var classes = $(data).find('[name="Lord"]');
+      // Si il trouve l'attribut name (correspond dans le fichier xml à <class name="">) alors il le stocke dans la variable
       console.log(classes)
       var talents = classes.children();
       var talent1 = talents[0];
