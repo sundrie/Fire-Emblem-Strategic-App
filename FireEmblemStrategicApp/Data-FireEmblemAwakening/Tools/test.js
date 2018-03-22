0639bbbdcc1ef06287b2f8e05242373a8a352667
unknown <sundrie42@gmail.com>
2018-03-22 22:23:37 +0100

    On ajoute bien les tr avec les noms de classes servant de class comme dans notre code originel avec nos txt

--- FireEmblemStrategicApp/Data-FireEmblemAwakening/Tools/test.js
+++ FireEmblemStrategicApp/Data-FireEmblemAwakening/Tools/test.js
@@ -22,11 +22,18 @@ $(document).ready(function(){
       // Fonction qui va afficher les talents dans notre page pour cela on boucle sur notre variable array
       console.log(classes[0].attr("name"))
       $(classes).each(function(i) {
-        $(".wrapper table tbody").append("<tr class="+classes[i].attr("name")+"></tr>")
+        // Si le nom de la classe issue du jeu contient un espace alors on supprime cet espace pour faire une seule classe et non 2 ou plus (ex : Great Knight donne normalement 2 class Great et Knight alors que l'on veut que ça soit une class unique GreatKnight)
+        // Sinon et bien on ajoute tout simplement sans traitement anti espace
+        if (/\s/.test(classes[i].attr("name"))) {
+          var removeSpace = classes[i].attr("name")
+          // On enlève les whitespace entre les mots (trim ne fonctionne qu'aux début et fin des strings)
+          removeSpace = removeSpace.replace(/\s/, '');
+          $(".wrapper table tbody").append("<tr class="+removeSpace+"></tr>")
+        } else {
+          $(".wrapper table tbody").append("<tr class="+classes[i].attr("name")+"></tr>")
+        }
       });
-      // var classes = $(data).find('[name="Lord"]');
-      // Si il trouve l'attribut name (correspond dans le fichier xml à <class name="">) alors il le stocke dans la variable
-      console.log(classes)
+      // console.log(classes)
       var talents = classes.children();
       var talent1 = talents[0];
       var talent2 = talents[1];
