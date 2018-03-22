86e18c8d87d18868515f8691e2b0352b9c3ee83f
unknown <sundrie42@gmail.com>
2018-03-22 21:26:17 +0100

    Nous avons une fonction qui certes pour l'instant ne cherche que la classe Lord mais ça va bientôt changer nous permets de récupérer toutes les données plus proprement et le tout en très peu de lignes de code comparé a ce que demande le décryptage d'un txt

--- FireEmblemStrategicApp/Data-FireEmblemAwakening/Tools/test.js
+++ FireEmblemStrategicApp/Data-FireEmblemAwakening/Tools/test.js
@@ -4,11 +4,20 @@ $(document).ready(function(){
     url: 'TalentsList.xml',
     datatype : 'xml',
     success: function(data){
-      // Variable qui liste toutes les classes 
+      // Variable qui liste toutes les classes
       var allclasses = $(data).children().children();
       console.log(allclasses)
-      // var classes = $(data).find('class[name="Lord"]');
-      // console.log(classes)
+      var classes
+      // On boucle sur chaque classes
+      $(allclasses).each(function(i) {
+        // Si il trouve l'attribut name (correspond dans le fichier xml à <class name="">) alors il le stocke dans la variable
+        if ($(this).attr("name") === "Lord") {
+          // console.log($(this))
+          classes = $(this);
+        }
+      });
+      // var classes = $(data).find('[name="Lord"]');
+      console.log(classes)
       var talents = classes.children();
       var talent1 = talents[0];
       var talent2 = talents[1];
