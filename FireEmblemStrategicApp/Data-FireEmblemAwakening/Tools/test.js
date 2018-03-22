8b07bb39e234e33e2abf4a4518f2be58d9131938
unknown <sundrie42@gmail.com>
2018-03-22 19:54:55 +0100

    J'ai créé plusieurs variables qui permettent de récupérer les 2 talents d'une classe

--- FireEmblemStrategicApp/Data-FireEmblemAwakening/Tools/test.js
+++ FireEmblemStrategicApp/Data-FireEmblemAwakening/Tools/test.js
@@ -1,10 +1,16 @@
 $(document).ready(function(){
+
   $.ajax({
     url: 'TalentsList.xml',
     datatype : 'xml',
     success: function(data){
       var classes = $(data).find('class[name="Lord"]');
       console.log(classes)
+      var talents = classes.children();
+      var talent1 = talents[0];
+      var talent2 = talents[1];
+      console.log(talent1)
+      console.log(talent2)
     }
   });
 });
