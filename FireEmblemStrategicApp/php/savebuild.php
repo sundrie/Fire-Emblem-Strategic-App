e18ce71641f04e42d9d35ec4c45782adeea555f6
unknown <sundrie42@gmail.com>
2018-07-06 16:40:45 +0200

    L'image du cinquième talent et sa position c'est fait !

--- FireEmblemStrategicApp/php/savebuild.php
+++ FireEmblemStrategicApp/php/savebuild.php
@@ -91,15 +91,21 @@ $destination_x = 185;
 $destination_y = 313.5;  // L'image du talent fait 23x23, sachant que le bloc 70 donc au centre c'est y départ + 23.5  
 imagecopy($destination, $source, $destination_x, $destination_y, 0, 0, $largeur_source, $hauteur_source);
 
+
 ImagefilledRectangle ($destination, 180, 370, 610, 440, $beige);
 $nomTalent = "Axebreaker";
 imagefttext($destination, 12, 0, 210, 390, $noir, $font, $nomTalent);
 $descTalent = "Précision et Esquive +50 si ennemi équipé d'une hache";
 imagefttext($destination, 10, 0, 210, 410, $noir, $font, $descTalent);
+$source = imagecreatefrompng("http://alexandreblin.ovh/FireEmblemStrategicApp/static/img/talents_icons/".$nomTalent.".png");
+$largeur_source = imagesx($source);
+$hauteur_source = imagesy($source);
+$destination_x = 185;
+$destination_y = 393.5;  // L'image du talent fait 23x23, sachant que le bloc 70 donc au centre c'est y départ + 23.5  
+imagecopy($destination, $source, $destination_x, $destination_y, 0, 0, $largeur_source, $hauteur_source);
 
 
-
-
+// Cette fonction envoie l'image une fois tout nos traitements faits
 imagepng($destination);
 
 // if($_POST){
