c93fb3bf288db71abb5fbe4fffa983434b4bb63e
unknown <sundrie42@gmail.com>
2018-07-06 16:26:56 +0200

    L'image du second talent et sa position c'est fait !

--- FireEmblemStrategicApp/php/savebuild.php
+++ FireEmblemStrategicApp/php/savebuild.php
@@ -49,7 +49,7 @@ $source = imagecreatefrompng("http://alexandreblin.ovh/FireEmblemStrategicApp/st
 $largeur_source = imagesx($source);
 $hauteur_source = imagesy($source);
 $destination_x = 185;
-$destination_y = 73.5;  // L'image du talent fait 23x23, sachant que le bloc 70 donc au centre c'est x départ + 23.5  
+$destination_y = 73.5;  // L'image du talent fait 23x23, sachant que le bloc 70 donc au centre c'est y départ + 23.5  
 imagecopy($destination, $source, $destination_x, $destination_y, 0, 0, $largeur_source, $hauteur_source);
 
 
@@ -58,6 +58,12 @@ $nomTalent = "Sol";
 imagefttext($destination, 12, 0, 210, 150, $noir, $font, $nomTalent);
 $descTalent = "(Tec)% de régénérer ses HP de la moitié des dégats infligés";
 imagefttext($destination, 10, 0, 210, 170, $noir, $font, $descTalent);
+$source = imagecreatefrompng("http://alexandreblin.ovh/FireEmblemStrategicApp/static/img/talents_icons/".$nomTalent.".png");
+$largeur_source = imagesx($source);
+$hauteur_source = imagesy($source);
+$destination_x = 185;
+$destination_y = 153.5;  // L'image du talent fait 23x23, sachant que le bloc 70 donc au centre c'est y départ + 23.5  
+imagecopy($destination, $source, $destination_x, $destination_y, 0, 0, $largeur_source, $hauteur_source);
 
 
 ImagefilledRectangle ($destination, 180, 210, 610, 280, $beige);
